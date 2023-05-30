c1376764658f428bcf0be296ecbe4b727ee4899a
Connor <61102529+Metater@users.noreply.github.com>
2023-05-30 13:00:48 -0400

    Modification of existing entities

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,12 +4,15 @@ using devDept.Eyeshot.Control;
 using devDept.Eyeshot.Control.Labels;
 using devDept.Eyeshot.Entities;
 using devDept.Geometry;
+using devDept.Graphics;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,6 +33,9 @@ namespace EyeshotLearning
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly CubeState state = new();
+        private readonly WorkManager<WorkUnit> workManager = new();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -41,12 +47,14 @@ namespace EyeshotLearning
 
         protected override void OnContentRendered(EventArgs e)
         {
+            SetDisplayMode(design1, displayType.Wireframe);
+
             design1.ProgressBar.Visible = false;
 
-            BuildCube build = new();
-            design1.StartWork(build);
+            design1.ActiveViewport.Labels.Add(new TextOnly(0.5, 0.5, 1.5, "Hello, World!", new Font("Tahoma", 16f, System.Drawing.FontStyle.Bold), System.Drawing.Color.Green, ContentAlignment.MiddleCenter));
 
-            design1.ActiveViewport.Labels.Add(new TextOnly(0, 0, 1, "Hello, World!", new Font("Tahoma", 8.25f, System.Drawing.FontStyle.Bold), System.Drawing.Color.Green));
+            workManager.AppendToQueue(new BuildCube(state));
+            workManager.RunAll(design1);
 
             base.OnContentRendered(e);
         }
@@ -66,32 +74,132 @@ namespace EyeshotLearning
             labelProgressBar.Content = "Completed";
         }
 
+        private void Button_Step_Click(object sender, RoutedEventArgs e)
+        {
+            workManager.AppendToQueue(new StepCube(state));
+            workManager.RunAll(design1);
+        }
+
         private void Button_Cancel_Click(object sender, RoutedEventArgs e)
         {
             design1.CancelWork();
         }
 
+        #region Design Config
+        private static void SetDisplayMode(Design design, displayType displayType)
+        {
+            design.ActiveViewport.DisplayMode = displayType;
+            SetBackgroundStyleAndColor(design);
+            design.UpdateBoundingBox(); // Updates simplified representation (when available)
+            design.Invalidate();
+        }
+        public static void SetBackgroundStyleAndColor(Design design)
+        {
+            design.ActiveViewport.CoordinateSystemIcon.Lighting = false;
+            design.ActiveViewport.ViewCubeIcon.Lighting = false;
+
+            switch (design.ActiveViewport.DisplayMode)
+            {
+
+                case displayType.HiddenLines:
+                    ((BackgroundSettings)design.ActiveViewport.Background).TopColor = RenderContextUtility.ConvertColor(System.Drawing.Color.FromArgb(0xD2, 0xD0, 0xB9));
+                    design.ActiveViewport.CoordinateSystemIcon.Lighting = true;
+                    design.ActiveViewport.ViewCubeIcon.Lighting = true;
+
+                    break;
+
+                default:
+                    ((BackgroundSettings)design.ActiveViewport.Background).TopColor = RenderContextUtility.ConvertColor(System.Drawing.Color.FromArgb(0xED, 0xED, 0xED));
+                    break;
+            }
+
+            design.CompileUserInterfaceElements();
+        }
+        #endregion
+
         private class BuildCube : WorkUnit
         {
-            public Solid? solid;
+            private readonly CubeState state;
+
+            public BuildCube(CubeState state)
+            {
+                this.state = state;
+            }
 
             public override void DoWork(BackgroundWorker worker, DoWorkEventArgs e)
             {
-                solid = Solid.CreateBox(1, 1, 1);
-                
+                state.Solid = Solid.CreateBox(1, 1, 1);
+                state.Text = new Text(0.5, 0.5, 1.01, "Hello, World!", 0.1, Text.alignmentType.MiddleCenter);
+                state.Circle = new Circle(0.5, 0.5, 1, 1);
             }
 
             public override void WorkCompleted(object sender)
             {
                 Workspace workspace = (Workspace)sender;
-                solid!.SmoothingAngle = Utility.DegToRad(45);
+                state.Solid!.SmoothingAngle = Utility.DegToRad(45);
 
-                workspace.Entities.Add(solid, System.Drawing.Color.Red);
-
-                Circle circle = new(0, 0, 1, 1);
-                workspace.Entities.Add(circle, System.Drawing.Color.Purple);
+                workspace.Entities.Add(state.Solid, System.Drawing.Color.Red);
+                workspace.Entities.Add(state.Text, System.Drawing.Color.Cyan);
+                workspace.Entities.Add(state.Circle, System.Drawing.Color.Purple);
 
                 workspace.ZoomFit();
+            }
+        }
+
+        private class StepCube : WorkUnit
+        {
+            private readonly CubeState state;
+
+            public StepCube(CubeState state)
+            {
+                this.state = state;
+            }
+
+            public override void DoWork(BackgroundWorker worker, DoWorkEventArgs e)
+            {
+                state.Text!.Translate(0, 0, 0.25);
+                state.Circle!.Scale(2);
+            }
+
+            public override void WorkCompleted(object sender)
+            {
+                Workspace workspace = (Workspace)sender;
+
+                workspace.Refresh();
+
+                workspace.ZoomFit();
+            }
+        }
+
+        public class CubeState
+        {
+            private Step step = Step.NegX;
+            public Solid? Solid { get; set; }
+            public Text? Text { get; set; }
+            public Circle? Circle { get; set; }
+
+            public bool TryStep(out Step nextStep)
+            {
+                if (step == Step.Done)
+                {
+                    nextStep = Step.Done;
+                    return false;
+                }
+
+                nextStep = (Step)(((int)step) + 1);
+                step = nextStep;
+                return true;
+            }
+
+            public enum Step
+            {
+                NegX,
+                NegY,
+                NegZ,
+                PosX,
+                PosY,
+                PosZ,
+                Done
             }
         }
     }
